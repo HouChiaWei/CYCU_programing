3ff6d98c5b67c2d37f2e57616d6a081d5f9c2f12
侯嘉韋 <72115876+HouChiaWei@users.noreply.github.com>
2022-10-12 16:43:14 +0000

    push updata

--- sheep_wolf_cabbage.py
+++ sheep_wolf_cabbage.py
@@ -16,3 +16,26 @@ No.7 : sheep  ->
 Compelete !
 
 '''
+#total: 15 times
+
+flag = 0
+boat_space = 2
+object = 4 # bull,rhino,goat,cabbage
+boat_driver = 2 # man,ape
+
+      # man,ape,bull,rhino,goat,cabbage
+east = [ 1 , 1 , 1 , 1 , 1 , 1 ]
+west = [ 0 , 0 , 0 , 0 , 0 , 0 ]
+sum_west = 0
+
+while west != object+boat_driver :
+    
+    for i in west :
+        sum_west += i
+
+    if sum_west == 6:
+        print("Compelete !")
+        print("Total :",flag," times.\n")
+        break 
+    
+    
